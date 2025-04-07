24ea757d0053ee630b52dd447e0c1525a7ceb0fe
mikelichina <lishuo94@126.com>
2025-04-08 07:50:59 +0800

    add loop

--- structCoding/Loop.java
+++ structCoding/Loop.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class Loop{
     public static void main(String[] args){
         /*
@@ -21,5 +23,22 @@ public class Loop{
             m=m+1;
         }while(m<=100);
         System.out.println(res);
+
+		int s=0;
+		for(int i=1;i<=100;i++){
+			s=s+i;
+		}
+		System.out.println(s);
+
+		ArrayList<Integer> al;
+		al=new ArrayList<Integer>();
+		al.add(1);
+		al.add(2);
+		al.add(3);
+		al.add(4);
+		al.add(5);
+		for(int a:al){
+			System.out.println(a);
+		}
     }
 }
