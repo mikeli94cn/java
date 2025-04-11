6d95ea5035e74c86fe9eb4ad6d562cc1589cd1a3
mikelichina <lishuo94@126.com>
2025-04-11 08:04:52 +0800

    123

---
+++ Calc.java
@@ -0,0 +1,15 @@
+public class Calc{
+	public static void main(String[] args){
+		System.out.println("please input calc expression:");
+		Scanner sc;
+		sc=new Scanner(System.in);
+		String str=sc.next();
+		/*
+	　　format expression judge and handle
+		1.clear blank char
+		2.only have 0-9,+ - * /
+		3.format is demical + - * / demical
+		*/
+
+	}
+}
