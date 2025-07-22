de2b787861f8dba51b39eb4c1f67fd5bc82b1d79
lishuo94 <lishuo94@126.com>
2025-07-22 02:53:21 +0000

    add Past.
    Signed-off-by: lishuo94 <lishuo94@126.com>

---
+++ Past/TestDivideZero.java
@@ -0,0 +1,14 @@
+public class TestDivideZero
+{
+    public static void main(String[] args)
+    {
+        int a;
+        //a=1/0;
+        //System.out.println(1/0);
+        
+        double b;
+        b=1.0/0.0;
+        System.out.println(b);
+
+    }
+}
