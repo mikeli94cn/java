1ea049f9fac971783b80f199309229e65fc64f90
lishuo94 <lishuo94@126.com>
2025-07-22 02:49:28 +0000

    add Past.
    Signed-off-by: lishuo94 <lishuo94@126.com>

---
+++ Past/PrintCalendar.java
@@ -0,0 +1,28 @@
+
+//获取当前日期
+//获取当前对应的星期数
+
+//获取当前月份
+//获取当前月份的天数
+//获取每天对应的星期数
+
+import static java.lang.System.*;
+import java.util.*;
+public class PrintCalendar{
+    public static void main(String[] args){
+        String today_date;
+        String today_week;
+
+        System.out.println(System.currentTimeMillis());
+        Date date1=new Date(System.currentTimeMillis());
+        date1.getTime();
+        out.println(date1.getTime());
+
+    }
+
+
+    
+
+
+
+}
