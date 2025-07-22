dad28047eceadbc7cfd14cb66c0e2c771ac3c64c
lishuo94 <lishuo94@126.com>
2025-07-22 02:48:34 +0000

    add Past.
    Signed-off-by: lishuo94 <lishuo94@126.com>

---
+++ Past/NewDog.java
@@ -0,0 +1,9 @@
+public class NewDog{
+    public static void main(String[] args){
+        Dog littleDog=new Dog();
+        littleDog.name="wangwang";
+        littleDog.height=30.6;
+        littleDog.bark();
+        littleDog.eat();
+    }
+}
