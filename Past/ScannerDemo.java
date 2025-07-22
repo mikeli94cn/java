22d0ee70eeb917d0804ce0c17299a15346e05edd
lishuo94 <lishuo94@126.com>
2025-07-22 02:51:26 +0000

    add Past.
    Signed-off-by: lishuo94 <lishuo94@126.com>

---
+++ Past/ScannerDemo.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+public class ScannerDemo{
+
+public static void main(String[] args){
+
+    Scanner sc;
+    sc=new Scanner(System.in);
+
+    /*
+    System.out.println("please input an string");
+    //read a new line
+    String s1=sc.nextLine();
+
+
+    System.out.println("please input an paragragh");
+    //read a paragragh
+    String s2= sc.next();
+
+    
+    //sc.nextInt();
+    //sc.nextDouble();
+    //sc.nextLong();
+    
+    System.out.println("input an integer:");
+    int i=sc.nextInt();
+
+    System.out.println("input an double:");
+    double double1=sc.nextDouble();
+
+    System.out.println("input a long");
+    long long1=sc.nextLong();
+    */
+
+
+    System.out.println("input an char:");
+    char c1=sc.next().charAt(0);
+
+
+
+    
+    //System.out.println(s1);
+    //System.out.println(s2);
+    //System.out.println(i);
+    //System.out.println(double1);
+    //System.out.println(long1);
+    System.out.println(c1);
+
+
+
+}
+
+
+}
