87172c774513325241d275118c77948b08632875
lishuo94 <lishuo94@126.com>
2025-07-23 00:34:45 +0000

    add fun_program_100/NineNineMulti0102.java.
    Signed-off-by: lishuo94 <lishuo94@126.com>

---
+++ fun_program_100/NineNineMulti0102.java
@@ -0,0 +1,13 @@
+package javalife.fun_program_100;
+
+public class NineNineMulti0102 {
+    public static void main(String[] args) {
+        for(int i=1;i<=9;i++){
+            for(int j=1;j<=i;j++){
+                System.out.printf("%d*%d=%d ",i,j,1*j);
+            }
+            System.out.print("\n");
+        }
+    }
+    
+}
