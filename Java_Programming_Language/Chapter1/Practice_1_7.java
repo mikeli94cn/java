de985dbaf23be1198c066ab115f8cecdf800e339
lishuo94 <lishuo94@126.com>
2025-07-24 12:05:17 +0000

    add Java_Programming_Language/Chapter1/Practice_1_7.java.
    Signed-off-by: lishuo94 <lishuo94@126.com>

---
+++ Java_Programming_Language/Chapter1/Practice_1_7.java
@@ -0,0 +1,9 @@
+package javalife.javapl.chapter1;
+public class Practice_1_7 {
+    public static void main(String[] args) {
+        for(int i=1;i<=100;i++)
+            System.out.println(i);
+        for(int i=100;i>=1;i--)
+            System.out.println(i);
+    }
+}
