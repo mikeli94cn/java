a22d65358295a18194a33caeb2d427b47c15cbf4
Mike Li <48801165+mikeli94cn@users.noreply.github.com>
2026-04-08 23:17:28 +0800

    Create ArithmeticBoolean.java

---
+++ ocf/01_handling_values/operator/ArithmeticBoolean.java
@@ -0,0 +1,20 @@
+package operator;
+
+public class ArithmeticBoolean {
+    /*
+    |category|opeartors|
+    |---|---|
+    |arithmetic|+,-,*,/,%|
+    |relational|==,!=,>,<,>=,<=,|
+    |logical|&,|,&&,|||
+
+    * short-curcuit: &&, ||
+    * &&: (false)&&(expr1), expr1 won't be executed; (true)&&(expr1), expr1 will be executed.
+    * ||: (true)||(expr1), expr1 NOT execute; (false)||(expr1), expr1 execute.
+
+    ### note
+    * arithmetic results are truncated in integer division (e.g., 5/2 is 2), unless you cast to double.
+    * boolean results are strictly true and false; java does not treat 0 or null as "false"
+     */
+
+}
