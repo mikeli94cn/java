c45fc6ec7b8cb1cd214b645083ae1d174738ac38
Mike Li <48801165+mikeli94cn@users.noreply.github.com>
2026-04-13 06:46:26 +0800

    Fix formatting of primitive and wrapper table

--- ocf/handling_values/primtivewrapper/PrimitiveWrapperTest.java
+++ ocf/handling_values/primtivewrapper/PrimitiveWrapperTest.java
@@ -5,16 +5,16 @@ import java.util.ArrayList;
 /*
 # primitive and wrapper
 ## list
-|primtive type|memory size|scope|wrapper class|
-|---|---|---|---|
-|byte|1|int -128~127|Byte|
-|short|2|int|Short|
-|int|4|int|Integer|
-|long|8|long int|Long|
-|float|4|float|Float|
-|double|8|double|Double|
-|boolean|1bit|true or false|Boolean|
-|char|2|character|Character|
+|primtive type|memory size|scope        |wrapper class|
+|---          |---        |---          |---          |
+|byte         |1          |int -128~127 |Byte         |
+|short        |2          |int          |Short        |
+|int          |4          |int          |Integer      |
+|long         |8          |long int     |Long         |
+|float        |4          |float        |Float        |
+|double       |8          |double       |Double       |
+|boolean      |1bit       |true or false|Boolean      |
+|char         |2          |character    |Character    |
 
 ## differ
 * memory: primitive type in stack memory (faster) , wrapper class objects in heap memory
