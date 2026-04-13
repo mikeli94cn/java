7e42622e0fad74e999726cd36cf8fbb6ff4a5c5f
Mike Li <48801165+mikeli94cn@users.noreply.github.com>
2026-04-13 12:40:33 +0800

    Update TextManipulation.java

--- ocf/handling_values/text/TextManipulation.java
+++ ocf/handling_values/text/TextManipulation.java
@@ -15,8 +15,21 @@ package handling_values.text;
   - best for: building Strings inside loops or complex dynamic text generation
   - key methods: append(), insert(), delete(), reverse()
   - thread safety: not thread-safe, use StringBuffer is synchronization is required
- */
 
+- text blocks (multi-line Strings)
+  - introduced in java 15, text blocks use triple quotes (""") to represent multi-line Strings.
+  - eliminates the need for most escape sequences (like " or \n), HTML\JSON\SQL
+  - key points:
+    1. start with """, end with """
+    2. start """ must follow by a newline, otherwise compile error; end with """ doesn't need to follow by a newline, if follow, a new line will print
+    3. text block is still String object, have all String properties and behaviours
+    4. text format based on the most front-est  column, other columns behind it will fill in whitespace.
+    5. if text block include a \, and \ is the line end, it will omit a new line (which means combine two lines)
+    6. other escape character normally print (such as \n)
+    7. text block cannot include """, if want to include """, need to add a escape character \ before any ".
+
+
+ */
 
 
 public class TextManipulation {
@@ -51,16 +64,100 @@ public class TextManipulation {
 
         String s4 = s2.trim().replace("alan turing", "von neumann");    //print von neumann
 
-        StringBuilder sbd1=new StringBuilder("hello,stringbuilder");
+        StringBuilder sbd1 = new StringBuilder("hello,stringbuilder");
         System.out.println(sbd1);    //print "hello,stringbuilder"
 
         sbd1.append('!');
         System.out.println(sbd1);    //print "hello,stringbuilder!"
 
+        sbd1.insert(0, 'j');
+        System.out.println(sbd1);    //print "jhello,stringbuilder!"
+
+        sbd1.insert(1, "ava,");
+        System.out.println(sbd1);    //print "java,hello,stringbuilder!"
+
+        sbd1.insert(5, 21);
+        System.out.println(sbd1);    //print "java,21hello,stringbuilder!"
+
+        sbd1.delete(5, 7);    //delete [5,7)
+        System.out.println(sbd1);    //print "java,hello,stringbuilder!"
+
+        sbd1.reverse();
+        System.out.println(sbd1);    //print "!redliubgnirts,olleh,avaj"
+
+        //1. start with """, end with """
+        String json = """
+                {
+                    "name":"java",
+                    "type":"language"
+                }
+                """;
+        System.out.println(json);
+
+        //3. text block is still String object, have all String properties and behaviours
+        String eyeTestText = """
+                "Java Study Guide"
+                  by Jeanne & Scott""";
+        System.out.println(eyeTestText);
+
+        String labelled = label("""
+                Java Study Guide
+                For java 21
+                2024 edition""", "Jeanne & Scott");
+        System.out.println(labelled);
+
+        String pyramid = """
+                  *
+                 * *
+                * * *""";
+        System.out.println(pyramid);
+
+        //2. start """ must follow by a newline, otherwise compile error; end with """ doesn't need to follow by a newline, if follow, a new line will print
+        //String block= """doe""";    //compile error, Illegal text block start: missing new line after opening quotes
+        String block = """
+                doe""";
+        System.out.println(block);
+
+        //5. if text block include a \, and \ is the line end, it will omit a new line (which means combine two lines)
+        String blockNew = """    
+                doe \
+                deer""";
+        System.out.println(blockNew);
+
+        //6. other escape character normally print (such as \n)
+        String blockTwo = """
+                \tdoe \n
+                deer""";
+        System.out.println(blockTwo);
+
+        //7. text block cannot include """, if want to include """, need to add a escape character \ before any ".
+        String blockThree = """
+                "doe\"\"\"
+                \"deer\"""
+                """;
+        System.out.println(blockThree);
+
+        String blockFour = """
+                  "doe\"""
+                 \"deer""\"
+                """;
+        System.out.println(blockFour);
+
+        //4. text format based on the most front-est  column, other columns behind it will fill in whitespace.
+        String blockFive = """
+                         hello
+                     java
+                scala""";
+        System.out.println(blockFive);
         
-
-
-
-
+        System.out.println("program end");
     }
+
+    //3. text block is still String object, have all String properties and behaviours
+    public static String label(String title, String author) {
+        return """
+                book:
+                """ + title + " by " + author;
+    }
+
 }
