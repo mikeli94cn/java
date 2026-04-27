c94cf0712eb4220931d311573a6821917451f993
mikeli94cn <lishuo94@126.com>
2026-04-27 12:55:19 +0800

    io

---
+++ ocp/io/ConsoleFileIOStreams.java
@@ -0,0 +1,32 @@
+/*
+ * using the java I/O api to read and write console and file data is one of the
+ * most fundamental tasks in java programming.
+ * this involves working with streams, which represent flows of data.
+ * */
+/*
+ * Console I/O (standard Input/Output)
+ * java provides built-in streams:
+ * System.in -> input (keyboard)
+ * System.out -> output (console)
+ * System.err -> error output
+ * */
+
+//reading from Console
+//Scanner
+
+//BufferedReader
+
+
+//writing to Console
+
+
+//File I/O
+//reading files
+//using FileInputStream
+
+//using BufferedReader
+
+//writing files
+//using FileOutPutStream
+
+//using BufferedWriter
