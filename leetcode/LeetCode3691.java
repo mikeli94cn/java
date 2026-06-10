afbfc91a5a11fdbdbada5166f38c89c29240e860
mikeli94cn <lishuo94@126.com>
2026-06-11 07:49:40 +0800

    leetcode3691

---
+++ leetcode/LeetCode3691.java
@@ -0,0 +1,48 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class LeetCode3691 {
+    public static void main(String[] args) {
+        int[] nums={4,2,5,1};
+        int k=3;
+        System.out.println(maxTotalValue(nums, k));
+
+    }
+
+    public static long maxTotalValue(int[] nums, int k){
+        //1.iterate +
+        //2.get subArr max,min +
+        //3.add to a list +
+        //4.sort and fetch top n value +
+
+        ArrayList<Integer> res=new ArrayList<>();
+        for(int i=1;i<=nums.length;i++){
+            int max=nums[i-1];
+            int min=nums[i-1];
+            for(int j=i+1;j<=nums.length;j++){
+                if(nums[j-1]>max){
+                    max=nums[j-1];
+                }
+
+                if(nums[j-1]<min){
+                    min=nums[j-1];
+                }
+                int value=max-min;
+                res.add(value);
+            }
+        }
+        res.sort(Collections.reverseOrder());
+
+        int n=0;
+        int sum=0;
+        for(int i : res){
+            sum=sum+i;
+            n++;
+            if(n==k){
+                break;
+            }
+        }
+
+        return sum;
+    }
+}
---
+++ ocp/aim.md
@@ -0,0 +1,6 @@
+data type, math calc, control flow
+basic dsa: arr, pointer
+oop
+functional
+std lib
+deploy tool
