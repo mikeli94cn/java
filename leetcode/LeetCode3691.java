8013287ecbedca9c29e009a7b2bca3789902587e
Mike Li <48801165+mikeli94cn@users.noreply.github.com>
2026-06-12 12:45:11 +0800

    Update LeetCode3691.java

--- leetcode/LeetCode3691.java
+++ leetcode/LeetCode3691.java
@@ -1,43 +1,47 @@
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.PriorityQueue;
 
 public class LeetCode3691 {
-    public static void main(String[] args) {
-        int[] nums={4,2,5,1};
-        int k=3;
-        System.out.println(maxTotalValue(nums, k));
+  public static void main(String[] args) {
+    int[] nums = {22};
+    int k = 1;
+    System.out.println(maxTotalValue(nums, k));
+  }
 
+  public static long maxTotalValue(int[] nums, int k) {
+    // 1.iterate +
+    // 2.get subArr max,min +
+    // 3.add to a list +
+    // 4.sort and fetch top n value +
+
+    PriorityQueue<Integer> res =
+        new PriorityQueue<>(Collections.reverseOrder());
+    if (nums.length == 1) {
+      res.add(0);
+    } else {
+      for (int i = 1; i <= nums.length; i++) {
+        int max = nums[i - 1];
+        int min = nums[i - 1];
+        for (int j = i + 1; j <= nums.length; j++) {
+          if (nums[j - 1] > max) {
+            max = nums[j - 1];
+          }
+
+          if (nums[j - 1] < min) {
+            min = nums[j - 1];
+          }
+          int value = max - min;
+          res.add(value);
+        }
+      }
     }
 
-        public static long maxTotalValue(int[] nums, int k){
-        //1.iterate +
-        //2.get subArr max,min +
-        //3.add to a list +
-        //4.sort and fetch top n value +
-
-        PriorityQueue<Integer> res = new PriorityQueue<>(Collections.reverseOrder());
-        for(int i=1;i<=nums.length;i++){
-            int max=nums[i-1];
-            int min=nums[i-1];
-            for(int j=i+1;j<=nums.length;j++){
-                if(nums[j-1]>max){
-                    max=nums[j-1];
-                }
-
-                if(nums[j-1]<min){
-                    min=nums[j-1];
-                }
-                int value=max-min;
-                res.add(value);
-            }
-        }
-
-        long sum=0;
-        for(int i=1;i<=k;i++){
-            sum=sum+res.peek();
-        }
-
-
-        return sum;
+    long sum = 0;
+    for (int i = 1; i <= k; i++) {
+      sum = sum + res.peek();
     }
+
+    return sum;
+  }
 }
