c97d3115e2f0a3974de24ddece13c857b0aff4b6
Mike Li <48801165+mikeli94cn@users.noreply.github.com>
2026-06-12 13:43:46 +0800

    Update LeetCode3691.java

--- leetcode/LeetCode3691.java
+++ leetcode/LeetCode3691.java
@@ -4,26 +4,26 @@ import java.util.PriorityQueue;
 
 public class LeetCode3691 {
   public static void main(String[] args) {
-    int[] nums = {22};
-    int k = 1;
+    int[] nums = {11, 8};
+    int k = 2;
     System.out.println(maxTotalValue(nums, k));
   }
 
   public static long maxTotalValue(int[] nums, int k) {
     // 1.iterate +
     // 2.get subArr max,min +
-    // 3.add to a list +
-    // 4.sort and fetch top n value +
+    // 3.add to a priority queue +
+    // 4.fetch top n value +
 
     PriorityQueue<Integer> res =
         new PriorityQueue<>(Collections.reverseOrder());
-    if (nums.length == 1) {
-      res.add(0);
-    } else {
-      for (int i = 1; i <= nums.length; i++) {
-        int max = nums[i - 1];
-        int min = nums[i - 1];
-        for (int j = i + 1; j <= nums.length; j++) {
+    for (int i = 1; i <= nums.length; i++) {
+      int max = nums[i - 1];
+      int min = nums[i - 1];
+      for (int j = i; j <= nums.length; j++) {
+        if (j == i) {
+          res.add(0);
+        } else {
           if (nums[j - 1] > max) {
             max = nums[j - 1];
           }
@@ -39,7 +39,7 @@ public class LeetCode3691 {
 
     long sum = 0;
     for (int i = 1; i <= k; i++) {
-      sum = sum + res.peek();
+      sum = sum + res.poll();
     }
 
     return sum;
