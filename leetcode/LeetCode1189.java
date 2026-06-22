6269acda1c31215483cbf3a2ee19bcc7556550ca
mikeli94cn <lishuo94@126.com>
2026-06-23 07:54:15 +0800

    1189

---
+++ leetcode/LeetCode1189.java
@@ -0,0 +1,34 @@
+import java.util.HashMap;
+import java.util.*;
+
+public class LeetCode1189 {
+
+    public static void main(String[] args) {
+
+        String text="nlaebolko";
+        System.out.println(maxNumberOfBallons(text));
+    }
+
+    public static int maxNumberOfBallons(String text){
+        char[] chs=text.toCharArray();
+        HashMap<Character,Integer> map=new HashMap<>();
+        for(char c:chs){
+            if(c=='b'||c=='a'||c=='l'||c=='o'||c=='n'){
+                if(map.containsKey(c)){
+                    map.put(c, map.get(c)+1);
+                }else{
+                    map.put(c, 1);
+                }
+            }
+        }
+        int maxNumber=0;
+        for(Map.Entry<Character,Integer> entry:map.entrySet() ){
+            if(entry.getKey()=='l'){
+                maxNumber=Math.max(maxNumber, entry.getValue()/2 );
+            }else{
+                maxNumber=Math.max(maxNumber, entry.getValue() );
+            }
+        }
+        return maxNumber;
+    }
+}
