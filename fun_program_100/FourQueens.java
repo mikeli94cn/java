d9ff27ab6bb2f25f806a5b172968db19613ecf13
Mike Li <48801165+mikeli94cn@users.noreply.github.com>
2026-06-22 19:46:25 +0800

    Update FourQueens.java

--- fun_program_100/FourQueens.java
+++ fun_program_100/FourQueens.java
@@ -1,100 +1,132 @@
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 
-public class FourQueens
-{
+public class FourQueens {
     public static void main(String[] args) {
-        int[] cols=new int[4];
-        //fourQueenOrigin(cols);
-        fourQueen(cols,1);
+        int[] res = new int[4];
+        // fourQueenOrigin(res);
+        // fourQueen(res, 1);
+        fourQueenDetail(res, 1, 1);
     }
 
-    public static void fourQueen(int[] cols,int n){
-        for(int i=1;i<=4;i++){
-            cols[n-1]=i;
-            if( n!=4 ){
-                if(checkXYAix(cols)){
-                    fourQueen(cols,n+1);
+  /*
+  public static void fourQueen(int[] res, int line) {
+    fourQueenDetail(res, 1, 1);
+
+    for (int i = 1; i <= 4; i++) {
+      res[n - 1] = i;
+      if (n != 4) {
+        if (checkXYAix(res)) {
+          fourQueen(res, n + 1);
+        }
+      } else {
+        if (checkXYAix(res)) {
+          for (int cnt = 1; cnt <= 4; cnt++) {
+            System.out.printf("(%d,%d)", cnt, res[cnt - 1]);
+          }
+          System.out.println();
+        }
+      }
+      res[n - 1] = 0;
+    }
+  }
+  */
+
+    public static void fourQueenDetail(int[] res, int col, int line) {
+        if (col >= 1 && col <= 4) {
+            if (line > 4) {
+                res[col-1]=0;
+                fourQueenDetail(res, col - 1, res[col - 1 -1] + 1);
+
+            } else {
+                res[col - 1] = line;
+
+                if (checkXYAix(res)) {
+                    if (col < 4) {
+                        fourQueenDetail(res, col + 1, 1);
+                    } else {
+                        System.out.println(Arrays.toString(res));
+                        fourQueenDetail(res, col, line + 1);
+                    }
+                } else {
+                    fourQueenDetail(res, col, line + 1);
                 }
-            }else{
-                if(checkXYAix(cols)){
-                    for(int cnt=1;cnt<=4;cnt++){
-                        System.out.printf("(%d,%d)",cnt,cols[cnt-1]);
+
+                res[col - 1] = 0;
+            }
+        }
+    }
+
+  /*
+  public static void fourQueenOrigin(int[] res) {
+    // four hierachy loops
+    for (int i = 1; i <= 4; i++) {
+      res[1 - 1] = i;
+      if (checkXYAix(res)) {
+        for (int j = 1; j <= 4; j++) {
+          res[2 - 1] = j;
+          if (checkXYAix(res)) {
+            for (int m = 1; m <= 4; m++) {
+              res[3 - 1] = m;
+              if (checkXYAix(res)) {
+                for (int n = 1; n <= 4; n++) {
+                  res[4 - 1] = n;
+                  if (checkXYAix(res)) {
+                    for (int cnt = 1; cnt <= 4; cnt++) {
+                      System.out.printf("(%d,%d)", cnt, res[cnt - 1]);
                     }
                     System.out.println();
+                  }
+                  res[4 - 1] = 0;
                 }
+              }
+              res[3 - 1] = 0;
             }
-            cols[n-1]=0;
+          }
+          res[2 - 1] = 0;
         }
+      }
+      res[1 - 1] = 0;
     }
+  }
+  */
 
-    public static void fourQueenOrigin(int[] cols){
-        //four hierachy loops
-        for(int i=1;i<=4;i++){
-            cols[1-1]=i;
-            if( checkXYAix(cols) ){
-                for(int j=1;j<=4;j++){
-                    cols[2-1]=j;
-                    if( checkXYAix(cols) ){
-                        for(int m=1;m<=4;m++){
-                            cols[3-1]=m;
-                            if( checkXYAix(cols) ){
-                                for(int n=1;n<=4;n++){
-                                    cols[4-1]=n;
-                                    if( checkXYAix(cols) ){
-                                        for(int cnt=1;cnt<=4;cnt++){
-                                            System.out.printf("(%d,%d)",cnt,cols[cnt-1]);
-                                        }
-                                        System.out.println();
-                                    }
-                                    cols[4-1]=0;
-                                }
-                            }
-                            cols[3-1]=0;
-                        }
-                    }
-                    cols[2-1]=0;
-                }
-            }
-            cols[1-1]=0;
-        }
-    }
-
-    public static boolean checkXYAix(int[] cols){
-        boolean flagPositive=true;
-        boolean flagNegative=true;
-        boolean flagHorizon=true;
-        HashSet<Integer> checkPositive=new HashSet<>();
-        HashSet<Integer> checkNegative=new HashSet<>();
-        HashSet<Integer> checkHorizon=new HashSet<>();
-        for(int i=1;i<=4;i++){
-            if(cols[i-1]!=0){
-                int tmpYminusX=cols[i-1]-i;
-                int tmpXplusY=i+cols[i-1];
-                if(checkPositive.contains(tmpYminusX)){
-                    flagPositive=false;
+    public static boolean checkXYAix(int[] res) {
+        boolean flagPositive = true;
+        boolean flagNegative = true;
+        boolean flagHorizon = true;
+        HashSet<Integer> checkPositive = new HashSet<>();
+        HashSet<Integer> checkNegative = new HashSet<>();
+        HashSet<Integer> checkHorizon = new HashSet<>();
+        for (int i = 1; i <= 4; i++) {
+            if (res[i - 1] != 0) {
+                int tmpYminusX = res[i - 1] - i;
+                int tmpXplusY = i + res[i - 1];
+                if (checkPositive.contains(tmpYminusX)) {
+                    flagPositive = false;
                     break;
-                }else{
+                } else {
                     checkPositive.add(tmpYminusX);
                 }
 
-                if(checkNegative.contains(tmpXplusY)){
-                    flagNegative=false;
+                if (checkNegative.contains(tmpXplusY)) {
+                    flagNegative = false;
                     break;
-                }else{
+                } else {
                     checkNegative.add(tmpXplusY);
                 }
 
-                if(checkHorizon.contains(cols[i-1])){
-                    flagHorizon=false;
+                if (checkHorizon.contains(res[i - 1])) {
+                    flagHorizon = false;
                     break;
-                }else{
-                    checkHorizon.add(cols[i-1]);
+                } else {
+                    checkHorizon.add(res[i - 1]);
                 }
             }
         }
 
-        return flagPositive&flagNegative&flagHorizon;
+        return flagPositive & flagNegative & flagHorizon;
     }
 }
