24573a7a156abb592bef54cdd3aed938b4d50531
Mike Li <48801165+mikeli94cn@users.noreply.github.com>
2026-06-23 18:06:15 +0800

    Update FourQueens.java

--- fun_program_100/FourQueens.java
+++ fun_program_100/FourQueens.java
@@ -1,97 +1,37 @@
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Map;
 
 public class FourQueens {
     public static void main(String[] args) {
         int[] res = new int[4];
-        // fourQueenOrigin(res);
-        // fourQueen(res, 1);
         fourQueenDetail(res, 1, 1);
     }
 
-  /*
-  public static void fourQueen(int[] res, int line) {
-    fourQueenDetail(res, 1, 1);
-
-    for (int i = 1; i <= 4; i++) {
-      res[n - 1] = i;
-      if (n != 4) {
-        if (checkXYAix(res)) {
-          fourQueen(res, n + 1);
-        }
-      } else {
-        if (checkXYAix(res)) {
-          for (int cnt = 1; cnt <= 4; cnt++) {
-            System.out.printf("(%d,%d)", cnt, res[cnt - 1]);
-          }
-          System.out.println();
-        }
-      }
-      res[n - 1] = 0;
-    }
-  }
-  */
-
     public static void fourQueenDetail(int[] res, int col, int line) {
         if (col >= 1 && col <= 4) {
-            if (line > 4) {
-                res[col-1]=0;
-                fourQueenDetail(res, col - 1, res[col - 1 -1] + 1);
-
-            } else {
+            if (line <= 4) {
                 res[col - 1] = line;
-
                 if (checkXYAix(res)) {
                     if (col < 4) {
                         fourQueenDetail(res, col + 1, 1);
                     } else {
                         System.out.println(Arrays.toString(res));
+                        res[col - 1] = 0;
                         fourQueenDetail(res, col, line + 1);
                     }
                 } else {
+                    res[col - 1] = 0;
                     fourQueenDetail(res, col, line + 1);
                 }
-
                 res[col - 1] = 0;
+            } else {
+                res[col - 1] = 0;
+                fourQueenDetail(res, col-1, res[col - 1 - 1] + 1);
+
             }
         }
     }
 
-  /*
-  public static void fourQueenOrigin(int[] res) {
-    // four hierachy loops
-    for (int i = 1; i <= 4; i++) {
-      res[1 - 1] = i;
-      if (checkXYAix(res)) {
-        for (int j = 1; j <= 4; j++) {
-          res[2 - 1] = j;
-          if (checkXYAix(res)) {
-            for (int m = 1; m <= 4; m++) {
-              res[3 - 1] = m;
-              if (checkXYAix(res)) {
-                for (int n = 1; n <= 4; n++) {
-                  res[4 - 1] = n;
-                  if (checkXYAix(res)) {
-                    for (int cnt = 1; cnt <= 4; cnt++) {
-                      System.out.printf("(%d,%d)", cnt, res[cnt - 1]);
-                    }
-                    System.out.println();
-                  }
-                  res[4 - 1] = 0;
-                }
-              }
-              res[3 - 1] = 0;
-            }
-          }
-          res[2 - 1] = 0;
-        }
-      }
-      res[1 - 1] = 0;
-    }
-  }
-  */
 
     public static boolean checkXYAix(int[] res) {
         boolean flagPositive = true;
