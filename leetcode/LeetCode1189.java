9da5307cb0f0ae54ed81f2ec67e25082aebc332a
Mike Li <48801165+mikeli94cn@users.noreply.github.com>
2026-06-23 09:29:15 +0800

    Update LeetCode1189.java

--- leetcode/LeetCode1189.java
+++ leetcode/LeetCode1189.java
@@ -1,34 +1,33 @@
-package org.example;
-
 import java.util.HashMap;
-import java.util.*;
+import java.util.Map;
 
 public class LeetCode1189 {
-
     public static void main(String[] args) {
-
-        String text="xyz";
-        System.out.println(maxNumberOfBallons(text));
+        String text = "xyz";
+        System.out.println(maxNumberBalloon(text));
     }
 
-    public static int maxNumberOfBallons(String text){
-        char[] chs=text.toCharArray();
-        HashMap<Character,Integer> map=new HashMap<>();
-        for(char c:chs){
-            if(c=='b'||c=='a'||c=='l'||c=='o'||c=='n'){
-                if(map.containsKey(c)){
-                    map.put(c, map.get(c)+1);
-                }else{
-                    map.put(c, 1);
-                }
+    public static int maxNumberBalloon(String text) {
+        HashMap<Character, Integer> res = new HashMap<>();
+        res.put('b', 0);
+        res.put('a', 0);
+        res.put('l', 0);
+        res.put('o', 0);
+        res.put('n', 0);
+        
+        char[] chs = text.toCharArray();
+        for (char c : chs) {
+            if (res.containsKey(c)) {
+                res.put(c, res.get(c) + 1);
             }
         }
-        int maxNumber=Integer.MAX_VALUE;
-        for(Map.Entry<Character,Integer> entry:map.entrySet() ){
-            if(entry.getKey()=='l'){
-                maxNumber=Math.min(maxNumber, entry.getValue()/2 );
-            }else{
-                maxNumber=Math.min(maxNumber, entry.getValue() );
+
+        int maxNumber = Integer.MAX_VALUE;
+        for (Map.Entry<Character, Integer> entry : res.entrySet()) {
+            if (entry.getKey() == 'l' || entry.getKey() == 'o') {
+                maxNumber = Math.min(maxNumber, entry.getValue() / 2);
+            } else {
+                maxNumber = Math.min(maxNumber, entry.getValue());
             }
         }
         return maxNumber;
