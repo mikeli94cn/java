28bcc9f52e320bceaa1fffaa3482920639b21d12
Mike Li <48801165+mikeli94cn@users.noreply.github.com>
2026-06-29 18:21:53 +0800

    Create LeetCode383.java

---
+++ leetcode/LeetCode383.java
@@ -0,0 +1,58 @@
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+public class LeetCode383 {
+    public static void main(String[] args) {
+        LeetCode383 test = new LeetCode383();
+        String ransomNote = "aa";
+        String magazine = "aab";
+        System.out.println(test.canConstruct(ransomNote, magazine));
+    }
+
+    public boolean canConstruct(String ransomNote, String magazine) {
+        char[] ranArr = ransomNote.toCharArray();
+        HashMap<Character, Integer> ranMap = new HashMap<>();
+        foo(ranArr, ranMap, ranArr.length);
+
+        char[] magArr = magazine.toCharArray();
+        HashMap<Character, Integer> magMap = new HashMap<>();
+        foo(magArr, magMap, magArr.length);
+
+        Iterator<Map.Entry<Character, Integer>> ranIter=ranMap.entrySet().iterator();
+        //return check(ranIter, magMap);
+        while(ranIter.hasNext()){
+            Map.Entry<Character, Integer> entry=ranIter.next();
+        }
+    }
+
+    public boolean check(Iterator<Map.Entry<Character, Integer>> ranIter, HashMap<Character, Integer> magMap){
+        if(ranIter.hasNext()){
+            Map.Entry<Character, Integer> entry=ranIter.next();
+            if(entry.getValue()> magMap.get(entry.getKey())){
+                return false;
+            }else {
+                return check(ranIter,magMap);
+            }
+        }else {
+            return true;
+        }
+    }
+
+    public void foo(char[] arr, HashMap<Character, Integer> map, int n) {
+        if (n > 1) {
+            foo(arr, map, n - 1);
+            safePut(map, arr[n - 1]);
+        } else {
+            safePut(map, arr[n - 1]);
+        }
+    }
+
+    public void safePut(HashMap<Character, Integer> map, char key) {
+        if (map.containsKey(key)) {
+            map.put(key, map.get(key) + 1);
+        } else {
+            map.put(key, 1);
+        }
+    }
+}
