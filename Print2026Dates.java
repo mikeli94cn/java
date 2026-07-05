de8abc20ef2c6a65a14063d2dd7c7d27e37f7b43
mikeli94cn <lishuo94@126.com>
2026-07-06 07:56:28 +0800

    shellsort

--- Print2026Dates.java
+++ Print2026Dates.java
@@ -1,12 +1,16 @@
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class Print2026Dates {
 
     public static void main(String[] args) {
         LocalDate date=LocalDate.of(2026, 1, 1);
+        DateTimeFormatter formatter=DateTimeFormatter.ofPattern("MM-dd yyyy");
+        String dateStr=date.format(formatter);
+        System.out.println(dateStr);
 
         while(date.getYear()==2026){
-            System.out.println(date);
+            //System.out.println(date);
             date=date.plusDays(1);
         }
     }
---
+++ fun_program_100/PrintOneToN.java
@@ -0,0 +1,8 @@
+public class PrintOneToN {
+
+    public static void printOneToN(int n){
+        
+        printOneToN(n-1);
+
+    }
+}
