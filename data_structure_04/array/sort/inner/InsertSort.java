393fedfebcc13816fbe12483da4bf5cc616a4261
Mike Li <48801165+mikeli94cn@users.noreply.github.com>
2026-07-06 16:29:40 +0800

    Update InsertSort.java

--- data_structure_04/array/sort/inner/InsertSort.java
+++ data_structure_04/array/sort/inner/InsertSort.java
@@ -1,1 +1,11 @@
-
+    public void insertSort(int[] arr) {
+        for (int i = 2; i <= arr.length; i++) {
+            int value = arr[i - 1];
+            int j = i - 1;
+            while (j >= 1 && arr[j - 1] > value) {
+                arr[j] = arr[j - 1];
+                j--;
+            }
+            arr[j + 1 - 1] = value;
+        }
+    }
