7da895f32d2fdfec9e3ae0abd56e264ece5786d1
Mike Li <48801165+mikeli94cn@users.noreply.github.com>
2026-09-02 16:49:42 +0800

    Create Test.java

---
+++ java_programming_language/04_data_structure/linked_list/Test.java
@@ -0,0 +1,50 @@
+
+public class Test {
+
+    public static void main(String[] args) {
+
+        /*
+        SingleLink tail = new SingleLink();
+
+        SingleLink node1 = new SingleLink();
+        node1.val = 5;
+        node1.next = tail;
+
+        SingleLink node2 = new SingleLink(4, node1);
+
+        SingleLink node3 = new SingleLink(3, node2);
+
+        SingleLink node4 = new SingleLink(2, node3);
+
+        SingleLink node5 = new SingleLink(1, node4);
+
+        SingleLink head = new SingleLink();
+        head.next = node5;
+         */
+        
+        SingleLink head=SingleLink.createLink(10);
+        
+        SingleLink.iterLink(head);
+
+        System.out.println(SingleLink.getLength(head));
+
+        SingleLink.insertNode(head, 99, 5);
+
+        SingleLink.iterLink(head);
+
+        //System.out.println(SingleLink.queryNodeByVal(head, 0));
+        //System.out.println(SingleLink.queryNodeByPos(head, 7));
+        System.out.println(SingleLink.deleteNodeByVal(head, 99));
+
+        //System.out.println(SingleLink.deleteNodeByPos(head, 6));
+        SingleLink.reverseLink(head);
+
+        SingleLink.iterLink(head);
+
+        SingleLink.sortLink(head);
+
+        SingleLink.iterLink(head);
+
+    }
+
+}
